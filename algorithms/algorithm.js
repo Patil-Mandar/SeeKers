acc0b38d3b8dfdf21939db28a46a0e33a2759398
Mandar <mvp23402@gmail.com>
2022-05-16 17:21:53 +0530

    added basic recommendation algo for user profile

--- algorithms/algorithm.js
+++ algorithms/algorithm.js
@@ -16,11 +16,22 @@ const isEligible = (user,job) => {
 
 
 //Returns condidate set(set of jobs whose preferense is to be found) for a user
-const getCandidateSet = (user) => {
+const getJobsCandidateSet = (user) => {
     let canditateSet = []
 
     Jobs.forEach((job)=>{
         if(isEligible(user,job)) canditateSet.push(job)
+    })
+
+    return canditateSet
+}
+
+//Returns condidate set(set of users whose preferense is to be found) for a job
+const getUsersCandidateSet = (job) => {
+    let canditateSet = []
+
+    Users.forEach((user)=>{
+        if(isEligible(user,job)) canditateSet.push(user)
     })
 
     return canditateSet
@@ -34,7 +45,7 @@ const preference = (user,job)=>{
 //returns k most recommended jobs for user
 const recommendKjobs = (user,k) =>{
     let preferenceList = {}
-    let canditateSet = getCandidateSet(user)
+    let canditateSet = getJobsCandidateSet(user)
     
     canditateSet.forEach((job)=>{
         preferenceList[job.id] = preference(user,job)
@@ -44,4 +55,16 @@ const recommendKjobs = (user,k) =>{
     return preferenceList.slice(0,k)
 }
 
-console.log(recommendKjobs(Users[0],2))
+//returns k most recommended jobs for user
+const recommendKusers = (job,k) => {
+    let preferenceList = {}
+    let canditateSet = getUsersCandidateSet(job)
+    
+    canditateSet.forEach((user)=>{
+        preferenceList[user.id] = preference(user,job)
+    })
+    preferenceList = Object.entries(preferenceList).sort((a,b) => b[1]-a[1])
+    
+    return preferenceList.slice(0,k)
+}
+
